e8fdc3cae7ab8b92d2f94495f27a9d53b02d6f12
Tiffani Rice <tiffanirice414@gmail.com>
2023-04-05 21:08:27 -0700

    Assignment Additions

--- js/app.js
+++ js/app.js
@@ -13,13 +13,13 @@ let firstQ = prompt('Do I like spicy food (Y / N)?').toLowerCase();
 console.log(firstQ);
 if (firstQ === 'y' || firstQ === 'yes') {
   correctAnswers.push(firstQ);
-  // console.log('Correct! The spicier, the better.');
+  console.log('Correct! The spicier, the better.');
   alert('Correct! The spicier, the better.');
 } else if (firstQ === 'n' || firstQ === 'no') {
-  // console.log('Wrong! I love spicy food.');
+  console.log('Wrong! I love spicy food.');
   alert('Wrong! I love spicy food.');
 } else {
-  // console.log('Sorry, buddy. You need to answer with a \'Y\' or a \'N\'');
+  console.log('Sorry, buddy. You need to answer with a \'Y\' or a \'N\'');
   alert('Sorry, buddy. You need to answer with a \'Y\' or a \'N\'');
 }
 
@@ -28,13 +28,13 @@ let secondQ = prompt('Would I describe myself as a social person? (Y / N)?').toL
 console.log(secondQ);
 if (secondQ === 'n' || secondQ === 'no') {
   correctAnswers.push(secondQ);
-  // console.log('Duh, social situations are scary');
+  console.log('Duh, social situations are scary');
   alert('Duh, social situations are scary');
 } else if (secondQ === 'y' || secondQ === 'yes') {
-  // console.log('I can come across as extroverted, but I am a huge introvert');
+  console.log('I can come across as extroverted, but I am a huge introvert');
   alert('I can come across as extroverted, but I am a huge introvert');
 } else {
-  // console.log('Sorry, pal. You need to answer with a \'Y\' or a \'N\'')
+  console.log('Sorry, pal. You need to answer with a \'Y\' or a \'N\'')
   alert('Sorry, pal. You need to answer with a \'Y\' or a \'N\'');
 }
 
@@ -42,13 +42,13 @@ let thirdQ = prompt('Do I own and use a surfboard? (Y / N)?').toLowerCase();
 console.log(thirdQ);
 if (thirdQ === 'y' || thirdQ === 'yes') {
   correctAnswers.push(thirdQ);
-  // console.log('Yep! I own and use a surfboard... Doesn\'t mean I am good at it though lol');
+  console.log('Yep! I own and use a surfboard... Doesn\'t mean I am good at it though lol');
   alert('Yep! I own and use a surfboard... Doesn\'t mean I am good at it though lol');
 } else if (thirdQ === 'n' || thirdQ === 'no') {
-  // console.log('Wronggg. I do own and use a surfboard in Florida.');
+  console.log('Wronggg. I do own and use a surfboard in Florida.');
   alert('Wronggg. I do own and use a surfboard in Florida.')
 } else {
-  // console.log('You need to answer with a \'Y\' or a \'N\'')
+  console.log('You need to answer with a \'Y\' or a \'N\'')
   alert('You need to answer with a \'Y\' or a \'N\'')
 }
 
@@ -56,14 +56,14 @@ if (thirdQ === 'y' || thirdQ === 'yes') {
 let fourthQ = prompt('I have two kids (Y / N)?').toLowerCase();
 console.log(fourthQ);
 if (fourthQ === 'y' || fourthQ === 'yes'){
-  // console.log('Yep! I have a daughter and a son.')
+  console.log('Yep! I have a daughter and a son.')
   correctAnswers.push(fourthQ);
   alert('Yep! I have a daughter and a son.');
 } else if (fourthQ === 'n' || fourthQ === 'no') {
-  // console.log('Nope, I have 2 children!');
+  console.log('Nope, I have 2 children!');
   alert('Nope, I have 2 children!');
 } else {
-  // console.log('You need a \'Y\' or a \'N\'')
+  console.log('You need a \'Y\' or a \'N\'');
   alert('You need a \'Y\' or a \'N\'');
 }
 
@@ -71,13 +71,13 @@ let fifthQ = prompt('Have I ever been on TV (Y / N)?').toLowerCase();
 console.log(fifthQ);
 if (fifthQ === 'n' || fifthQ === 'no') {
   correctAnswers.push(fifthQ);
-  // console.log('You\'re right! i\'m not interesting enough to be on TV');
+  console.log('You\'re right! i\'m not interesting enough to be on TV');
   alert('You\'re right! i\'m not interesting or toxic enough to be on TV lol');
 } else if (fifthQ === 'y' || fifthQ === 'yes') {
-  // console.log('Wrong! I've never been on TV');
+  console.log('Wrong! I\'ve never been on TV');
   alert('Wrong! I am not interesting or outgoing enough');
 } else {
-  // console.log('Sorry, buddy. You need to answer with a \'Y\' or a \'N\'');
+  console.log('Sorry, buddy. You need to answer with a \'Y\' or a \'N\'');
   alert('You need to answer with a \'Y\' or a \'N\'');
 }
 
